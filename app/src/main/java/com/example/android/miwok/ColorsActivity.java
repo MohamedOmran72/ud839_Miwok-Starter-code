a1a3e9692f06e665398ecb01b06aa5750e0a4633
Mohamed Omran <mohamed_omran72@outlook.com>
2020-02-04 23:29:48 +0200

    Add explicit intents to open view

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,4 +12,6 @@ public class ColorsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
     }
+
+
 }
